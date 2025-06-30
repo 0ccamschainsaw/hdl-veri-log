27bd6c8c35a306a91e2a1b4b0d3b46e773157700
Akshatt Uppal <akshatt.uppal@gmail.com>
2025-06-30 21:09:32 +0530

    Create lfsr5.v

---
+++ Registers/lfsr5.v
@@ -0,0 +1,20 @@
+//linear feedback shift register is lfsr. (1 bit is maintained using xor)
+module top_module(
+    input clk,
+    input reset,    // Active-high synchronous reset to 5'h1
+    output [4:0] q
+); 
+    always @(posedge clk) begin
+        if (reset) begin
+            q <= 5'h1;
+        end
+        else begin
+            q[4] <= 1'b0 ^ q[0];
+            q[3] <= q[4];
+            q[2] <= q[3] ^ q[0];
+            q[1] <= q[2];
+            q[0] <= q[1];
+        end
+    end
+
+endmodule
