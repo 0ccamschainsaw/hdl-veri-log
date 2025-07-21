c3010072cad6bcd3b1551312041f677ac8422e37
Akshatt Uppal <akshatt.uppal@gmail.com>
2025-07-21 19:00:45 +0530

    Create q6c_fsm.v
    Signed-off-by: Akshatt Uppal <akshatt.uppal@gmail.com>

---
+++ 2.Sequential_logic/FSMs/q6c_fsm.v
@@ -0,0 +1,10 @@
+module top_module (
+    input [6:1] y,
+    input w,
+    output Y2,
+    output Y4);
+    parameter A = 1, B = 2, C = 3, D = 4, E = 5, F = 6;
+    //y2 and y4 are B AND D
+    assign Y2 = (y[A] & ~w);
+    assign Y4 = (y[B] & w) | (y[C] & w) | (y[E] & w) | (y[F] & w);
+endmodule
