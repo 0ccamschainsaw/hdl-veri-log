d68754dc2623bc45a492e65464fb0c1f5f21c797
Akshatt Uppal <akshatt.uppal@gmail.com>
2025-07-21 21:35:00 +0530

    Update counter_1000.v
    Signed-off-by: Akshatt Uppal <akshatt.uppal@gmail.com>

--- 2a. Building Larger Circuits/counter_1000.v
+++ 2a. Building Larger Circuits/counter_1000.v
@@ -1,1 +1,14 @@
-
+module top_module (
+    input clk,
+    input reset,
+    output [9:0] q);
+    always @(posedge clk) begin
+        if (reset) begin
+            q <= 0;
+        end
+        else begin
+            if (q < 999) q <= q + 1;      //999 not the same as 9'd999
+            else q <= 0;
+        end
+    end
+endmodule
